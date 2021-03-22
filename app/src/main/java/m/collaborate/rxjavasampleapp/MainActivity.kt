f888c5552fe8b0d5ceafe4278e23a97ad763b3aa
dlqkfzlr <hisunwoong@gmail.com>
2021-03-22 21:07:17 +0900

    created an android studio project

---
+++ app/src/main/java/m/collaborate/rxjavasampleapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package m.collaborate.rxjavasampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import m.collaborate.rxjavasampleapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
