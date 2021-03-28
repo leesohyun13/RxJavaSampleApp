9be13481a88b0e6f00e5c68b2d038b2e537f237c
thgus5614@naver.com <thgus5614@naver.com>
2021-03-28 19:20:40 +0900

    Add service interface that get image from kakao search

---
+++ data/src/main/java/m/collaborate/data/network/KakaoImageService.kt
@@ -0,0 +1,30 @@
+package m.collaborate.data.network
+
+import m.collaborate.data.remote.enums.KakaoSearchSortType
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoImageService {
+    @GET(SUB_PATH_BOOK)
+    fun searchDocuments(
+        @Header(SCHEMA_REQUEST_HEADER_AUTHORIZATION) restApiKey: String = REST_API_KEY,
+        @Query(SCHEMA_QUERY) query: String,
+        @Query(SCHEMA_QUERY_SORT) sort: KakaoSearchSortType = KakaoSearchSortType.ACCURACY,
+        @Query(SCHEMA_QUERY_PAGE) page: Int = BOOK_STARTING_PAGE_INDEX,
+        @Query(SCHEMA_QUERY_SIZE) size: Int = BOOK_PAGING_SIZE,
+    )
+
+    companion object {
+        const val REST_API_KEY = "KakaoAK 343fa18e242a2a558f3d5998a683f061"
+        const val SUB_PATH_BOOK = "v3/search/image"
+        const val BOOK_STARTING_PAGE_INDEX = 1
+        const val BOOK_PAGING_SIZE = 50
+
+        const val SCHEMA_REQUEST_HEADER_AUTHORIZATION = "Authorization"
+        const val SCHEMA_QUERY = "query"
+        const val SCHEMA_QUERY_SORT = "sort"
+        const val SCHEMA_QUERY_PAGE = "page"
+        const val SCHEMA_QUERY_SIZE = "size"
+    }
+}
