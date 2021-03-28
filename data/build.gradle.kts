eb4153df3700a01d9c0cdc8d077cb4243015d529
thgus5614@naver.com <thgus5614@naver.com>
2021-03-28 20:53:31 +0900

    Change gson library version

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:3.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.31.2-alpha")
