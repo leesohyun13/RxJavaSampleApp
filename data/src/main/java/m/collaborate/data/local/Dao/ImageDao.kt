85ea498c130b7f30d778fc358d368b799a64b9ca
thgus5614@naver.com <thgus5614@naver.com>
2021-04-03 10:18:08 +0900

    Change return data format on ImageDao.kt's select function

--- data/src/main/java/m/collaborate/data/local/Dao/ImageDao.kt
+++ data/src/main/java/m/collaborate/data/local/Dao/ImageDao.kt
@@ -1,8 +1,9 @@
 package m.collaborate.data.local.Dao
 
+import androidx.paging.DataSource
 import androidx.room.*
+import androidx.room.Dao
 import m.collaborate.data.local.entity.Image
-import javax.sql.DataSource
 
 @Dao
 interface ImageDao {
@@ -11,7 +12,7 @@ interface ImageDao {
     suspend fun insertAllImages(books: List<Image>)
 
     @Query("SELECT * FROM image")
-    fun selectPagedBooks(): List<Image> // FIXME add paging
+    fun selectPagedBooks(): DataSource.Factory<Int, Image>
 
     @Query("DELETE FROM image")
     suspend fun deleteImages()
