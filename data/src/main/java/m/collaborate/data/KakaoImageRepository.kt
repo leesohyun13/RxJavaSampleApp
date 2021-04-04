96a863de0e1bea94cbd36e7a18f2b48d5a851a7a
thgus5614@naver.com <thgus5614@naver.com>
2021-04-04 19:06:13 +0900

    Add remoteKey's function

--- data/src/main/java/m/collaborate/data/KakaoImageRepository.kt
+++ data/src/main/java/m/collaborate/data/KakaoImageRepository.kt
@@ -1,17 +1,21 @@
 package m.collaborate.data
 
-import androidx.lifecycle.LiveData
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 import m.collaborate.data.local.entity.Image
+import m.collaborate.data.local.entity.RemoteKey
+import m.collaborate.data.remote.enums.KakaoSearchSortType
 import m.collaborate.data.remote.model.KakaoImageResponse
 import m.collaborate.data.remote.model.wrapper.ResWrapper
 
 interface KakaoImageRepository {
 
-    suspend fun searchImage(searchText: String): ResWrapper<KakaoImageResponse>
-
-    fun getImages(): LiveData<List<Image>>
+    suspend fun searchImage(query: String, page: Int, sort: KakaoSearchSortType?): ResWrapper<KakaoImageResponse>
+    fun searchBookStream(query: String, target: KakaoSearchSortType?): Flow<PagingData<Image>>
 
     suspend fun saveImages(images: List<Image>)
-
     suspend fun clearImages()
+    suspend fun saveRemoteKeys(remoteKeys: List<RemoteKey>)
+    suspend fun getRemoteKeyWithIsbn(isbn: String): RemoteKey?
+    suspend fun clearRemoteKeys()
 }
--- data/src/main/java/m/collaborate/data/KakaoImageRepositoryImpl.kt
+++ data/src/main/java/m/collaborate/data/KakaoImageRepositoryImpl.kt
@@ -1,30 +1,58 @@
 package m.collaborate.data
 
-import androidx.lifecycle.LiveData
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 import m.collaborate.data.local.LocalDataSource
 import m.collaborate.data.local.entity.Image
-import m.collaborate.data.remote.model.KakaoImageResponse
+import m.collaborate.data.local.entity.RemoteKey
+import m.collaborate.data.network.KakaoImageApi
+import m.collaborate.data.paging.ImageSearchRemoteMediator
 import m.collaborate.data.remote.RemoteDataSource
-import m.collaborate.data.remote.model.wrapper.ResWrapper
+import m.collaborate.data.remote.enums.KakaoSearchSortType
+import m.collaborate.data.remote.network.NetworkExt
 import javax.inject.Inject
 
+@OptIn(ExperimentalPagingApi::class)
 class KakaoImageRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource
-) : KakaoImageRepository {
-    override suspend fun searchImage(searchText: String): ResWrapper<KakaoImageResponse> {
-        TODO("Not yet implemented")
+) : KakaoImageRepository, NetworkExt() {
+    override suspend fun searchImage(query: String, page: Int, sort: KakaoSearchSortType?) = safeAPi {
+        remoteDataSource.searchImages(query, page, sort)
     }
 
-    override fun getImages(): LiveData<List<Image>> {
-        TODO("Not yet implemented")
+    override fun searchBookStream(
+        query: String,
+        target: KakaoSearchSortType?,
+    ): Flow<PagingData<Image>> {
+        val pagingSourceFactory = { localDataSource.getPagedImages() }
+        return Pager(
+            config = PagingConfig(pageSize = KakaoImageApi.BOOK_STARTING_PAGE_INDEX, enablePlaceholders = false),
+            remoteMediator = ImageSearchRemoteMediator(query, target, remoteDataSource, localDataSource),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
     }
 
     override suspend fun saveImages(images: List<Image>) {
-        TODO("Not yet implemented")
+        localDataSource.insertImages(images)
     }
 
     override suspend fun clearImages() {
-        TODO("Not yet implemented")
+        localDataSource.deleteImages()
+    }
+
+    override suspend fun saveRemoteKeys(remoteKeys: List<RemoteKey>) {
+        localDataSource.insertOrReplace(remoteKeys)
+    }
+
+    override suspend fun getRemoteKeyWithIsbn(imageUrl: String): RemoteKey? {
+        return localDataSource.remoteKeyById(imageUrl)
+    }
+
+    override suspend fun clearRemoteKeys() {
+        localDataSource.clearRemoteKeys()
     }
 }
