f1ebbe85c81272e594c7fe21ac3ba68bc6a69765
thgus5614@naver.com <thgus5614@naver.com>
2021-04-04 19:33:04 +0900

    Change NetworkExt.kt format

--- data/src/main/java/m/collaborate/data/remote/network/NetworkExt.kt
+++ data/src/main/java/m/collaborate/data/remote/network/NetworkExt.kt
@@ -5,20 +5,17 @@ import kotlinx.coroutines.withContext
 import m.collaborate.data.remote.model.wrapper.ResWrapper
 import retrofit2.HttpException
 
-abstract class NetworkExt {
-
-    suspend fun <T : Any> safeAPi(apiCall: suspend () -> T): ResWrapper<T> {
-        return withContext(Dispatchers.IO) {
-            try {
-                ResWrapper.Success(apiCall.invoke())
-            } catch (t: Throwable) {
-                when(t) {
-                    is HttpException -> {
-                        ResWrapper.Error(isNetworkError = false, errorCode = t.code(), errorBody = t.response()?.errorBody())
-                    }
-                    else -> {
-                        ResWrapper.Error(true, null, null)
-                    }
+suspend fun <T : Any> safeAPi(apiCall: suspend () -> T): ResWrapper<T> {
+    return withContext(Dispatchers.IO) {
+        try {
+            ResWrapper.Success(apiCall.invoke())
+        } catch (t: Throwable) {
+            when(t) {
+                is HttpException -> {
+                    ResWrapper.Error(isNetworkError = false, errorCode = t.code(), errorBody = t.response()?.errorBody())
+                }
+                else -> {
+                    ResWrapper.Error(true, null, null)
                 }
             }
         }
