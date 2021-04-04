6f18d624cffff7677e953f79e3971315f6121fc9
thgus5614@naver.com <thgus5614@naver.com>
2021-04-04 19:13:30 +0900

    Add MainAActivity.kt

---
+++ app/src/main/java/m/collaborate/rxjavasampleapp/MainAActivity.kt
@@ -0,0 +1,12 @@
+package m.collaborate.rxjavasampleapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainAActivity : AppCompatActivity(R.layout.activity_main_a) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
