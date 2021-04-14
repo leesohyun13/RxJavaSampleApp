32f39889c36f9ddf6ea01ada3593dad01ee5daab
thgus5614@naver.com <thgus5614@naver.com>
2021-04-14 22:19:08 +0900

    Remove fragment and Add viewBinding and viewmodel

--- app/src/main/java/m/collaborate/rxjavasampleapp/MainActivity.kt
+++ app/src/main/java/m/collaborate/rxjavasampleapp/MainActivity.kt
@@ -1,19 +1,21 @@
 package m.collaborate.rxjavasampleapp
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import m.collaborate.rxjavasampleapp.ui.main.MainFragment
+import m.collaborate.rxjavasampleapp.databinding.MainActivityBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
+    private lateinit var binding : MainActivityBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.inputSearchText.hint = "검색어를 입력하세요."
     }
 }
