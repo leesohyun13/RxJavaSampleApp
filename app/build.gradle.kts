a7f9a76ed36cd07336203f178ec5bf0f858764d0
thgus5614@naver.com <thgus5614@naver.com>
2021-04-14 22:16:09 +0900

    Add data module on app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":data"))
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
